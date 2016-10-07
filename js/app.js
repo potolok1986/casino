d7ba1604f9456a528de3b59d954ea5723c8e2d63
al.v.kuzmin <al.v.kuzmin@s7.ru>
2016-10-07 15:16:20 +0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var rotate = angular.module("wheelFortune",[]);
---
+++ js/controllers/mainCtrl.js
@@ -0,0 +1,4 @@
+rotate.controller("mainCtrl",[function () {
+    var main = this;
+    main.colors = ["Orange","Violet","Sapphirine","Blue","Green","Yellow","Red","Black"]
+}]);
