ef2a5f51aabc885e2c0d3933db31415d12409e78
Александр <fnpal@mail.ru>
2016-10-10 21:31:52 +0700

    Внес косметические правки и в коде порядо навел

--- js/directives/drWheel.js
+++ js/directives/drWheel.js
@@ -1,70 +1,68 @@
 rotate.directive("drWheel", ["$timeout", function ($timeout) {
+    var Animation = function (circles, delayInMilliseconds) {
+        this.circles = circles * 360;
+        this.delay = delayInMilliseconds * 1000;
+        this.angleRotateWrap = 0;
+        this.angleRotate = 0;
+        this.oldAngle = 0;
+    };
+    Animation.prototype.animate = function (options, callback) {
+        var start = performance.now();
+        requestAnimationFrame(function animate(time) {
+            var timeFraction = (time - start) / options.duration,
+                progress;
+            if (timeFraction > 1) timeFraction = 1;
+            progress = options.timing(timeFraction);
+            options.draw(progress);
+            timeFraction < 1 ? requestAnimationFrame(animate) : $timeout(callback);
+        });
+    };
+    Animation.prototype.bounce = function (timeFraction) {
+        for (var a = 0, b = 1; 1; a += b, b /= 2) {
+            if (timeFraction >= (7 - 4 * a) / 11) {
+                return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
+            }
+        }
+    };
+    Animation.prototype.makeEaseOut = function (timing) {
+        return function (timeFraction) {
+            return 1 - timing(1 - timeFraction);
+        }
+    };
+    Animation.prototype.calculateAngleRotation = function(newAngle) {
+        console.log(newAngle, this.oldAngle);
+        this.angleRotate = newAngle >= this.oldAngle ? newAngle - this.oldAngle : 360 + (newAngle - this.oldAngle)
+    };
+
     return {
         templateUrl: 'directives/drWheel.html',
-        scope:{
-            angle:"@drWheel"
+        scope: {
+            angle: "@drWheel"
         },
         link: function postLink($scope, elem) {
             var $wheel = elem.find("img"),
                 $container = elem.find("div"),
-                angleRotateWrap = 0,
-                angleRotate = 0,
-                oldAngle = 0,
-                animateDuration = 6000,
-                bounceEaseOut = makeEaseOut(bounce);
+                animation = new Animation(3,6);
 
             $scope.getAngle = function () {
                 rotateWheel(parseInt($scope.angle) || 0);
             };
 
             function rotateWheel(newAngle) {
-                angleRotate = calculateAngleRotation(newAngle);
-                $container[0].style.transform = "rotate(" + angleRotateWrap + "deg)";
-                angleRotateWrap = newAngle;
+                animation.calculateAngleRotation(newAngle);
+                $container[0].style.transform = "rotate(" + animation.angleRotateWrap + "deg)";
+                animation.angleRotateWrap = newAngle;
                 $scope.waiting = true;
-                animate({
-                    duration: animateDuration,
-                    timing: bounceEaseOut,
+                animation.animate({
+                    duration: animation.delay,
+                    timing: animation.makeEaseOut(animation.bounce),
                     draw: function (progress) {
-                        $wheel[0].style.transform = "rotate(" + progress * (angleRotate + 720) + "deg)";
+                        $wheel[0].style.transform = "rotate(" + progress * (animation.angleRotate + animation.circles) + "deg)";
                     }
-                });
-                $timeout(function () {
-                    oldAngle = newAngle;
+                }, function () {
+                    animation.oldAngle = newAngle;
                     $scope.waiting = false;
-                },animateDuration + 30)
-
-            }
-
-            function calculateAngleRotation(newAngle) {
-                console.log(newAngle,oldAngle);
-                return newAngle >= oldAngle ? newAngle - oldAngle : 360 + (newAngle - oldAngle)
-            }
-            function animate(options) {
-                var start = performance.now();
-                requestAnimationFrame(function animate(time) {
-                    var timeFraction = (time - start) / options.duration;
-                    if (timeFraction > 1) timeFraction = 1;
-                    var progress = options.timing(timeFraction);
-                    options.draw(progress);
-                    if (timeFraction < 1) {
-                        requestAnimationFrame(animate);
-                    }
                 });
-            }
-
-            function bounce(timeFraction) {
-                for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
-                    if (timeFraction >= (7 - 4 * a) / 11) {
-                        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
-                    }
-                }
-            }
-
-            function makeEaseOut(timing) {
-                return function (timeFraction) {
-                    return 1 - timing(1 - timeFraction);
-                }
             }
         }
     }
